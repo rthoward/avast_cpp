b47fc94d788f60a3725a009d45dc1aa3b6e569d4
a-out <richbone227@gmail.com>
2013-08-03 20:10:54 -0400

    implemented Pickable::use

--- src/pickable.cpp
+++ src/pickable.cpp
@@ -1,10 +1,23 @@
+#include "libtcod.hpp"
 #include "actor.hpp"
+#include "container.hpp"
+#include "engine.hpp"
 #include "pickable.hpp"
 
 bool Pickable::pick(Actor *me, Actor *wearer) {
-   if (me->getContainer() && wearer->getContainer->add(me)) {
-      engine.getActorList->remove(me);
+   if (me->getContainer() && wearer->getContainer()->add(me)) {
+      engine.getActorList().remove(me);
       return true;
    }
+   return false;
+}
+
+bool Pickable::use(Actor *me, Actor *wearer) {
+   if (wearer->getContainer()) {
+      wearer->getContainer()->remove(me);
+      delete me;
+      return true;
+   }
+   
    return false;
 }
