d403ed682ce49935785fb0be309b3e9c79b99da4
a-out <richbone227@gmail.com>
2013-08-11 17:32:25 -0400

    proper combat messages
    - player damage in red, all else in white

--- src/attacker.cpp
+++ src/attacker.cpp
@@ -1,8 +1,11 @@
 #include "libtcod.hpp"
+#include "engine.hpp"
 #include "destructible.hpp"
+#include "gui.hpp"
 #include "actor.hpp"
 #include "attacker.hpp"
-#include <iostream>
+#include <string>
+#include <sstream>
 using namespace std;
 
 Attacker::Attacker(float power) : power(power) {
@@ -14,13 +17,22 @@ void Attacker::attack(Actor *attacker, Actor *target) {
    float damageTaken = 0;
    Attacker *att = attacker->getAttacker();
    Destructible *dest = target->getDestructible();
+   ostringstream attack_msg, dmg_msg;
+   attack_msg<< attacker->getName() << " attacks " << target->getName();
+   engine.getGUI()->message(attack_msg.str(), TCODColor::white);
 
    // make sure target is destructible and alive
    if (target->getDestructible() && !target->getDestructible()->isDead()) {
       if ( (damageTaken = dest->takeDamage(target, att->getPower())) ) {
-         // attack message
+         dmg_msg << target->getName() << " takes " << damageTaken;
+         dmg_msg << " points of damage!";
+         if (target == engine.getPlayer())
+            engine.getGUI()->message(dmg_msg.str(), TCODColor::darkerRed);
+         else
+            engine.getGUI()->message(dmg_msg.str(), TCODColor::white);
       } else {
-
+         dmg_msg << attacker->getName() << " misses!";
+         engine.getGUI()->message(dmg_msg.str(), TCODColor::white);
       }
    }
 }
